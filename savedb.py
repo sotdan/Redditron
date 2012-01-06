30d4bea1855edfcab1d60f8d4be6c9aea16d9639
sotdan <sotdan7@gmail.com>
2012-01-07 00:28:12 +0200

    db as txt

--- savedb.py
+++ savedb.py
@@ -5,7 +5,7 @@ import sys, MySQLdb
 mysqlhost = 'localhost'
 mysqluser = 'sotdan'
 mysqlpassword = 'neckbeard'
-database = 'ircbot'
+database = 'redditron'
 
 
 db = MySQLdb.connect(host=mysqlhost, user=mysqluser, 
@@ -14,15 +14,13 @@ cursor = db.cursor()
 cursor.execute("SELECT id FROM responses")
 keys = cursor.fetchall()
 
-f = open('scriptz/irc-bot/db', 'w')
+f = open('db.txt', 'w')
 for x in keys:
     cursor.execute(
-        "SELECT keyword FROM responses WHERE id = '{!s}'"
-                    .format(x[0]))
+        "SELECT keyword FROM responses WHERE id = '%s'" % (x[0]))
     key = str(cursor.fetchall()[0][0])
     cursor.execute(
-        "SELECT response FROM responses WHERE id = '{!s}'"
-                    .format(x[0]))
+        "SELECT response FROM responses WHERE id = '%s'" %(x[0]))
     response = str(cursor.fetchall()[0][0])
     print key
     f.write(key+' --- '+response+'\n')
