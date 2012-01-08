215405270d857029d7bd53f3db26d3314382d94a
Daniel Panayi <sotdan@sotdan-desktop.(none)>
2012-01-08 17:33:38 +0100

    update

--- importfromfile.py
+++ importfromfile.py
@@ -2,12 +2,29 @@ import sys, MySQLdb
 
 #imports to the db from a text file
 
+
+BOTADMIN = 'sotdan'
 mysqlhost = 'localhost'
 mysqluser = 'sotdan'
 mysqlpassword = 'neckbeard'
 database = 'redditron'
 
-f = open('scriptz/irc-bot/db', 'r')
+f = open('db.txt', 'r')
+
+
+def setbotadmin(nick):
+    dbcmd("INSERT INTO botadmins VALUES ('%s')" % (nick))
+
+def dbcmd(msg):
+    try:
+        db = MySQLdb.connect(host=mysqlhost, user=mysqluser, 
+                         passwd=mysqlpassword, db=database)
+        cursor = db.cursor()
+        cursor.execute(msg)
+        return cursor.fetchall()
+    except:
+        print "database error"
+        return ()
 
 for line in f:
     msg = line.split(" --- ")
@@ -15,8 +32,8 @@ for line in f:
         tag=msg[0]
         response=msg[1].rstrip()
         response=response.replace("'","\\'")
-        db = MySQLdb.connect(host=mysqlhost, user=mysqluser, 
-                                 passwd=mysqlpassword, db=database)
-        cursor = db.cursor()
         print 'adding: "'+response+'" with the tag '+tag
-        cursor.execute("INSERT INTO responses VALUES ('', '%s', '%s')" % (tag,response))
+        dbcmd("INSERT INTO responses VALUES ('', '%s', '%s')" % (tag,response))
+        
+setbotadmin(BOTADMIN)
+
