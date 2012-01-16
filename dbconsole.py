beb0f4f233e725dd4b41a7046896ccc8abd460e8
sotdan <sotdan7@gmail.com>
2012-01-16 11:43:08 +0100

    new linesplit function and an improved dbconsole

---
+++ dbconsole.py
@@ -0,0 +1,121 @@
+import sys, cPickle, responses
+'''
+loads quotes from a txt file to a pickle file'''
+
+
+RESPONSES = responses.Responses()
+
+
+def writetotxtfile(args):
+    if len(args)>0:
+        filename = args[0]
+        f = open(sys.path[0]+'/'+filename, 'w')
+        f.write(RESPONSES.getstring())
+        print 'done.'
+        f.close()
+    else: 
+        print 'writes responses to a txt file'
+        print 'needs a filename as an argument'
+
+def addresponses(args):
+    if len(args)>0:
+        filename = args[0]
+        f = open(sys.path[0]+'/'+filename, 'r')
+        f.write(RESPONSES.getstring())
+        print 'done.'
+    else:
+        print 'adds responses from a txt file'
+        print 'needs filename as an argument'
+        return
+    errors,new,alreadyexist=0,0,0
+    for line in f:
+        msg = line.split(" --- ")
+        if len(msg) == 2:
+            tag=msg[0]
+            response=msg[1].rstrip()
+            rc=RESPONSES.add(tag,response)
+            if rc==0:
+                errors+=1
+            elif rc==1: 
+                new+=1
+            elif rc==2:
+                alreadyexist+=1
+    RESPONSES.savetofile()
+    print '%s error(s) while adding' %str(errors)
+    print 'Added %s new quote(s).' % str(new)
+    print '%s quote(s) already existed' % str(alreadyexist)
+    stats('')
+
+def reviewquotesbyqid(qids):
+    for qid in qids:
+        quote= RESPONSES.getquoteforqid(qid[0],qid[1])
+        if quote==[]:
+            pass
+        else:
+            print '\n'+quote[0].upper()
+            print quote[1]+'\n'
+            msg= raw_input('>> enter d to delete or q to return: ')
+            if msg == 'd' or msg == 'D':
+                RESPONSES.deletequoteprompt(qid[0],qid[1])
+            elif msg == 'q':
+                print 'exiting review'
+                return    
+
+
+def reviewquotes(args):
+    reviewquotesbyqid(RESPONSES.getallqids())
+
+def stats(args):
+    print RESPONSES.stats()
+
+def search(args):
+    if len(args) == 0:
+        print 'needs an argument'
+    else:
+        lit=''
+        if len(args)>1:
+            for arg in args[:len(args)-1]:
+                lit+=arg+' '
+        lit+=args[len(args)-1]
+        result=RESPONSES.searchdb(lit)
+        print 'found %s matches' % len(result)
+        for x in result:
+            print 'Quote ID: '+str(x[0])
+            print x[1]+' --- '+x[2]
+        if len(result)>0:
+            msg= raw_input('>> enter r to review these quotes: ')
+            if msg=='r':
+                qids=[]
+                for x in result:
+                    qids.append(x[0])
+                reviewquotesbyqid(qids)
+
+def delete(args):
+    if len(args) == 2:
+        if args[0].isdigit() and args[1].isdigit():
+            RESPONSES.deletequoteprompt(int(args[0]), int(args[1]))
+        else: print 'args need to be digits'
+    else: print 'needs args'
+
+def main():
+    while True:
+        msg= raw_input('>>')
+        if msg == u'q':
+            sys.exit("Bye")
+        if msg == u'help':
+            print 'stats | addresponses | writetotxtfile | reviewquotes'
+        else:
+            args=[]
+            if len(msg.split())>1:
+                msg=msg.split()
+                args=msg[1:]
+                msg=msg[0]
+            msg = msg+'(args)'
+            #eval(msg)
+            try: eval(msg)
+            except: 
+                print sys.exc_info()[1]
+
+
+if __name__ == '__main__': 
+   main()
---
+++ responses.py
@@ -0,0 +1,173 @@
+import cPickle as pickle
+import random, sys
+
+class Responses(object):
+   
+   def __init__(self):
+       self.quotes = self.loaddb()
+
+   def loaddb(self):
+       x=''
+       try:
+           output=open(sys.path[0]+"/responses.dat")
+           x=pickle.load(output)
+           output.close()
+       except:pass
+       if isinstance(x, dict):
+           return x
+       else: 
+           print 'responsefile is not a dict'
+           return {}
+
+
+   def savetofile(self):
+       if self.quotes != self.loaddb():
+           print 'saving.'
+           output = open(sys.path[0]+"/responses.dat", 'wb')
+           pickle.dump(self.quotes, output)
+           output.close()
+       else: print 'nothing new to save'
+
+   def getkeys(self):
+       r=''
+       for x in self.quotes.keys():
+           r += x+'\n'
+       return r.strip()
+
+   def stats(self):
+       tags=self.quotes.keys()
+       q = 0
+       for x in tags:
+           if self.quotes[x] == None:
+               print 'removing '+x
+               del self.quotes[x]
+           else:
+               q+=len(self.quotes[x])
+       return 'There are '+str(len(tags))+' tags and '+str(q)+' quotes.'
+
+   def detect(self, msg):
+        response, logmsg="",""
+        keys = self.quotes.keys()
+        detected=[]
+        for key in keys:
+            if key in msg:
+                detected.append(key)
+        if len(detected)>0:
+            key = random.choice(detected)
+            if self.quotes[key] ==None:
+                response = 'error'
+            else: 
+                responselist = self.quotes[key]
+                response = random.choice(responselist)
+        return detected, response
+
+   def getresponses(self, msg):
+       '''Returns all the responses for a tag'''
+       response =""
+       try:
+           responselist = self.quotes[msg]
+           response='Quotes about %s:\n\n' % msg.upper()
+           for q in responselist:
+               response+=q+'\n\n'
+           return True, response
+       except: return False, "No quotes about '%s'" % msg
+
+   def getresponseslist(self):
+       '''returns a list with all the responses'''
+       r=[]
+       for x in self.quotes.keys():
+           if self.quotes[x] != None:
+               for y in self.quotes[x]:
+                   r.append(y)
+       return r
+
+   def getstring(self):
+       '''Returns the db as a fancy string'''
+       r=""
+       for x in self.quotes.keys():
+           if self.quotes[x] != None:
+               for y in self.quotes[x]:
+                   r+=(x+' --- '+y+'\n\n')
+       return r
+   
+   def searchdb(self, lit):
+       '''Returns a list of qid-tag-quote lists'''
+       result=[]
+       xid=0
+       for x in self.quotes.keys():
+           xid+=1
+           if self.quotes[x] != None:
+               yid=0
+               for y in self.quotes[x]:
+                   yid+=1
+                   if lit in y:
+                       qid=[xid,yid]
+                       result.append([qid,x,y])
+       return result
+   
+   def getallqids(self):
+       '''returns a list of all quote IDs'''
+       result=[]
+       xid=0
+       for x in self.quotes.keys():
+           xid+=1
+           if self.quotes[x] != None:
+               yid=0
+               for y in self.quotes[x]:
+                   yid+=1
+                   result.append([xid,yid])
+       return result
+
+   def getquoteforqid(self,tagid,responseid):
+       xid=0
+       result=[]
+       for x in self.quotes.keys():
+           xid+=1
+           if xid==tagid:
+               if self.quotes[x] != None:
+                   yid=0
+                   for y in self.quotes[x]:
+                       yid+=1
+                       if responseid==yid:
+                           result= [x,y]
+       return result
+                       
+   def deletequote(self,xid,yid):
+       quote=self.getquoteforqid(xid,yid)
+       self.quotes[quote[0]].remove(quote[1])
+       self.savetofile()                        
+
+   def deletequoteprompt(self,tagid,responseid):
+       quote= self.getquoteforqid(tagid,responseid)
+       if quote==[]:
+           print 'quote not found'
+       else:
+           print 'are you sure want to delete: '
+           print quote[0]+' - '+quote[1]
+           print '? (y/n)'
+           msg= raw_input('>>')
+           if msg =='y' or msg=='Y':
+               self.deletequote(tagid,responseid)
+           else: print 'aborted.'
+
+   def randomquote(self):
+       vals=self.quotes.values()
+       r = random.choice(vals)
+       return random.choice(r)
+  
+   def add(self, tag, response):
+       '''return codes:
+       0: error - 1: added - 2: quote already exists
+       '''
+       tag=tag.lower()
+       if tag in self.quotes:
+           if self.quotes[tag]==None:
+               return 0
+           else:
+               if not response in self.quotes[tag]:
+                   self.quotes[tag].append(response)
+                   return 1
+               else: return 2
+       else:
+           self.quotes[tag] = [response]
+           return 1
